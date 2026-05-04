9993505ab59464e7237a731fe6e2e8db0ccc2769
Gustavo Zorzo <guzorzo@gmail.com>
2026-05-04 15:48:15 -0300

    fix: remove unused parameter from server listen callback

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { buildServer } from "@infra/server/index.js";
 const port = process.env.PORT ? Number(process.env.PORT) : 3008;
 const server = buildServer();
 
-server.listen({ port, host: "0.0.0.0" }, function (err, address) {
+server.listen({ port, host: "0.0.0.0" }, function (err) {
   if (err) {
     server.log.error(err);
     process.exit(1);
