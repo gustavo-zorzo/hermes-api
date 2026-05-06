1b87a597d988d24dacf75a40b3ed6048b631f85f
Gustavo Zorzo <guzorzo@gmail.com>
2026-05-06 16:14:51 -0300

    feat: add OrderCreatedEvent class to handle order creation events

---
+++ src/domain/events/OrderCreatedEvent.ts
@@ -0,0 +1,29 @@
+import { DomainEvent } from "../../lib/DomainEvent.js";
+
+type OrderCreatedEventProps = {
+  id: string;
+  customer: {
+    name: string;
+    document: { value: string; type: "CPF" | "CNPJ" };
+    address: {
+      street: string;
+      neighbourhood: string;
+      city: string;
+      state: string;
+      zipcode: string;
+    };
+    email: string;
+  };
+  items: { productId: string; quantity: number; price: number }[];
+  status: string;
+  totalPrice: number;
+};
+
+export class OrderCreatedEvent extends DomainEvent<OrderCreatedEventProps> {
+  constructor(payload: OrderCreatedEventProps) {
+    super(OrderCreatedEvent.eventName(), payload);
+  }
+  static eventName() {
+    return "order.created";
+  }
+}
