60ac372c8957d570d2242cfa7a0006f08cde9a4c
Gustavo Zorzo <guzorzo@gmail.com>
2026-05-06 16:15:34 -0300

    feat: add isCreation flag and method to Entity class; introduce EventMapper interface

--- src/lib/Entity.ts
+++ src/lib/Entity.ts
@@ -5,15 +5,21 @@ export type EntityProps<T> = {} & T;
 export abstract class Entity<T> {
   private readonly $props: T;
   private readonly $id: EntityIDVO;
+  private readonly $isCreation: boolean;
 
   constructor(props: T, id?: EntityIDVO) {
     this.$id = id || new EntityIDVO();
     this.$props = props;
+    this.$isCreation = !id;
   }
   get id(): EntityIDVO {
     return this.$id;
   }
   get props(): T {
     return this.$props;
+  }
+
+  protected isCreation(): boolean {
+    return this.$isCreation;
   }
 }
