a4008b1f0f795422e840f0c79b888364c9dc9ae7
Gustavo Zorzo <guzorzo@gmail.com>
2026-05-07 14:44:10 -0300

    feat: implement OrderCancelledEvent class to handle order cancellation events

---
+++ src/domain/events/OrderCancelledEvent.ts
@@ -0,0 +1,28 @@
+import { DomainEvent } from "../../lib/DomainEvent.js";
+
+type OrderCancelledEventProp = {
+  id: string;
+  customer: {
+    id: string;
+    name: string;
+    document: { value: string; type: "CPF" | "CNPJ" };
+    address: {
+      street: string;
+      neighbourhood: string;
+      city: string;
+      state: string;
+      zipcode: string;
+    };
+    email: string;
+  };
+  items: { productId: string; quantity: number; price: number }[];
+  totalPrice: number;
+};
+export class OrderCancelledEvent extends DomainEvent<OrderCancelledEventProp> {
+  constructor(payload: OrderCancelledEventProp) {
+    super(OrderCancelledEvent.eventName(), payload);
+  }
+  static eventName() {
+    return "order.cancelled";
+  }
+}
